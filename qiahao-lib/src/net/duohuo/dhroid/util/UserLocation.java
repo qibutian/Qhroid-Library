ab32643c324a3f40f8f99dde8c144047290f96f7
hu845643018 <845643018@qq.com>
2017-12-27 22:00:54 +0800

    升级google定位

--- qiahao-lib/src/net/duohuo/dhroid/util/UserLocation.java
+++ qiahao-lib/src/net/duohuo/dhroid/util/UserLocation.java
@@ -1,18 +1,20 @@
 package net.duohuo.dhroid.util;
 
 import android.content.Context;
-import android.location.Location;
-import android.os.Bundle;
 import android.util.Log;
 
 import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
-import com.amap.api.location.LocationManagerProxy;
-import com.amap.api.location.LocationProviderProxy;
 
 public class UserLocation implements AMapLocationListener {
 
-    private LocationManagerProxy mLocationManagerProxy;
+    //声明AMapLocationClient类对象
+    public AMapLocationClient mLocationClient = null;
+    //声明AMapLocationClientOption对象
+    public AMapLocationClientOption mLocationOption = null;
+
 
     private String city = "南京", provice;
 
@@ -27,8 +29,8 @@ public class UserLocation implements AMapLocationListener {
     private String street = "";
     OnLocationChanged onLocationChanged;
     private static double EARTH_RADIUS = 6378.137;
-    
-    AMapLocation  aMapLocation;
+
+    AMapLocation aMapLocation;
 
 
     public static UserLocation getInstance() {
@@ -39,32 +41,65 @@ public class UserLocation implements AMapLocationListener {
     }
 
     public void init(Context context) {
-        mLocationManagerProxy = LocationManagerProxy.getInstance(context);
-        mLocationManagerProxy.setGpsEnable(true);
-        // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
-        // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用removeUpdates()方法来取消定位请求
-        // 在定位结束后，在合适的生命周期调用destroy()方法
-        // 其中如果间隔时间为-1，则定位只定一次,
-        // 在单次定位情况下，定位无论成功与否，都无需调用removeUpdates()方法移除请求，定位sdk内部会移除
-        mLocationManagerProxy.requestLocationData(
-                LocationProviderProxy.AMapNetwork, -1, 15, this);
+        //初始化定位
+        mLocationClient = new AMapLocationClient(context);
+        //设置定位回调监听
+        mLocationClient.setLocationListener(this);
+        //初始化AMapLocationClientOption对象
+        mLocationOption = new AMapLocationClientOption();
+        /**
+         * 设置定位场景，目前支持三种场景（签到、出行、运动，默认无场景）
+         */
+//        mLocationOption.setLocationPurpose(AMapLocationClientOption.AMapLocationPurpose.SignIn);
+        //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+
+        //获取一次定位结果：
+        //该方法默认为false。
+//        mLocationOption.setOnceLocation(true);
+
+        //获取最近3s内精度最高的一次定位结果：
+        //设置setOnceLocationLatest(boolean b)接口为true，启动定位时SDK会返回最近3s内精度最高的一次定位结果。如果设置其为true，setOnceLocation(boolean b)接口也会被设置为true，反之不会，默认为false。
+        mLocationOption.setOnceLocationLatest(true);
+
+        if (null != mLocationClient) {
+            mLocationClient.setLocationOption(mLocationOption);
+            //设置场景模式后最好调用一次stop，再调用start以保证场景模式生效
+            mLocationClient.stopLocation();
+            mLocationClient.startLocation();
+        }
+
     }
 
 
     public void init(Context context, int locationTime) {
-        mLocationManagerProxy = LocationManagerProxy.getInstance(context);
-        mLocationManagerProxy.setGpsEnable(true);
-        // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
-        // 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用removeUpdates()方法来取消定位请求
-        // 在定位结束后，在合适的生命周期调用destroy()方法
-        // 其中如果间隔时间为-1，则定位只定一次,
-        // 在单次定位情况下，定位无论成功与否，都无需调用removeUpdates()方法移除请求，定位sdk内部会移除
-        mLocationManagerProxy.requestLocationData(
-                LocationProviderProxy.AMapNetwork, locationTime, 15, this);
+        //初始化定位
+        mLocationClient = new AMapLocationClient(context);
+        //设置定位回调监听
+        mLocationClient.setLocationListener(this);
+        //初始化AMapLocationClientOption对象
+        mLocationOption = new AMapLocationClientOption();
+        /**
+         * 设置定位场景，目前支持三种场景（签到、出行、运动，默认无场景）
+         */
+//        mLocationOption.setLocationPurpose(AMapLocationClientOption.AMapLocationPurpose.SignIn);
+        //设置定位模式为AMapLocationMode.Hight_Accuracy，高精度模式。
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+
+        //设置定位间隔,单位毫秒,默认为2000ms，最低1000ms。
+        mLocationOption.setInterval(locationTime);
+
+        if (null != mLocationClient) {
+            mLocationClient.setLocationOption(mLocationOption);
+            //设置场景模式后最好调用一次stop，再调用start以保证场景模式生效
+            mLocationClient.stopLocation();
+            mLocationClient.startLocation();
+        }
+
     }
 
     public void cancleLocation() {
-        mLocationManagerProxy.removeUpdates(this);
+        mLocationClient.stopLocation();
     }
 
     public String getCity() {
@@ -122,35 +157,9 @@ public class UserLocation implements AMapLocationListener {
     public void setDistrict(String district) {
         this.district = district;
     }
-    
-    public  AMapLocation  getAmapLocation () {
-    	return aMapLocation;
-    }
-    
 
-
-    @Override
-    public void onLocationChanged(Location arg0) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void onProviderDisabled(String arg0) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void onProviderEnabled(String arg0) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
-        // TODO Auto-generated method stub
-
+    public AMapLocation getAmapLocation() {
+        return aMapLocation;
     }
 
 
@@ -169,8 +178,8 @@ public class UserLocation implements AMapLocationListener {
     @Override
     public void onLocationChanged(AMapLocation amapLocation) {
         if (amapLocation != null
-                && amapLocation.getAMapException().getErrorCode() == 0) {
-        	aMapLocation = amapLocation;
+                && amapLocation.getErrorCode() == 0) {
+            aMapLocation = amapLocation;
             islocation = true;
             // 定位成功回调信息，设置相关消息
             city = amapLocation.getCity();
@@ -197,9 +206,11 @@ public class UserLocation implements AMapLocationListener {
             latitude = amapLocation.getLatitude();
             longitude = amapLocation.getLongitude();
         } else {
+            //定位失败时，可通过ErrCode（错误码）信息来确定失败的原因，errInfo是错误信息，详见错误码表。
+            Log.e("AmapError", "location Error, ErrCode:"
+                    + amapLocation.getErrorCode() + ", errInfo:"
+                    + amapLocation.getErrorInfo());
             islocation = false;
-            Log.e("AmapErr", "Location ERR:"
-                    + amapLocation.getAMapException().getErrorCode());
         }
     }
 
